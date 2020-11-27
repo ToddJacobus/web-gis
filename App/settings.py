f2e001a3e927dbcf807416943a46795e93fd7c19
Todd Jacobus <rtjacobus1@gmail.com>
2020-11-27 09:12:40 -1000

    fixed settings, local sh file

--- App/settings.py
+++ App/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
 DEBUG = True if str(os.environ.get('DEBUG')).lower() == "true" else False
 
 # SECURITY WARNING: don't run with debug turned on in production!
